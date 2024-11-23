b8a4056d624ce9923dbe94c245e6ea047d623985
mcosandovalr <mco.sandovalr@gmail.com>
2024-11-23 14:39:08 -0600

    added enemies to demo_world and added movement to red_spirit
    added path to map and rearranged some trees
    
    - added movement to red_spirit
    - added 3 red_spirits to demo_world with their markers

--- scripts/red_spirit.gd
+++ scripts/red_spirit.gd
@@ -1,11 +1,24 @@
 extends CharacterBody2D
 
-@export var speed: float = 150.0
+@export var speed = 20
+@export var limit = 0.5
+@export var end_point: Marker2D
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-func handle_input():
-	var direction = Input.get_vector("ui_left", "ui_right","ui_up","ui_down")
-	velocity = direction * speed
+var start_position
+var end_position
+
+func _ready():
+	start_position = position
+	end_position = end_point.global_position
+
+
+func change_direction():
+	var temp = end_position
+	end_position = start_position
+	start_position = temp
+	
 
 func update_animation():
 	if velocity.length() == 0:
@@ -22,7 +35,15 @@ func update_animation():
 		#print("animation to play: ", animation_to_play)
 		animation_player.play(animation_to_play)
 
+
+func update_velocity():
+	var move_direction = end_position - position
+	velocity = move_direction.normalized() * speed
+	if move_direction.length() < limit:
+		change_direction()
+
+
 func _physics_process(delta: float) -> void:
-	handle_input()
+	update_velocity()
 	move_and_slide()
 	update_animation()
